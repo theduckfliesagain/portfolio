500dc84eb4e23a5f930706b9f33e8a3fd5669ce4
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-03-11 18:10:55 +0000

    add basic animations and restyle

--- bundle.js
+++ bundle.js
@@ -1,17 +1,67 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const anime = require('animejs');
-const { delay } = require('lodash');
+const { reverse } = require('lodash');
 
+
+const pageTitle = document.getElementById('page-title');
+pageTitle.innerHTML = pageTitle.innerHTML.split(' ')
+                        .map(word =>`<span>${word}</span>`)
+                        .reduce((str, word) => str + " " + word);
+
+
+const introArrow = document.getElementById('arrow');
+
+const navbar = document.querySelector("nav");
+
+introArrow.addEventListener('click', () => {
+    navbar.scrollIntoView();
+})
+                        
 anime({
     targets: '.item',
     // translateX: 250,
     rotate: '1turn',
     backgroundColor: 'rgb(255, 255, 255)',
     duration: 800,
-    delay: function(el, i, l) {
+    delay: function (el, i, l) {
         return i * 100;
-      },
-  });
+    },
+});
+
+anime({
+    targets: 'section',
+    translateX: 250,
+    rotate: '',
+    backgroundColor: '#1800cf',
+    duration: 800,
+    autoplay: false,
+    direction: reverse,
+    delay: function (el, i, l) {
+        return i * 100;
+    },
+});
+
+
+anime({
+    targets: '#page-title span',
+    // translateX: { value: -100, direction: reverse},
+    duration: 1600,
+    // direction: 'reverse',
+    easing: 'easeInQuad',
+    delay: anime.stagger(200),
+    opacity: 100,
+});
+
+
+
+
+anime({
+    targets: '#arrow',
+    translateY: 25,
+    duration: 1600,
+    direction: 'alternate',
+    loop: true,
+});
 
 
 },{"animejs":2,"lodash":3}],2:[function(require,module,exports){
