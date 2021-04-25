9e0a069b7c9987fe54d5935fb4c0fe2dfa907efd
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-25 11:58:18 +0100

    add error rendering

--- js/github.js
+++ js/github.js
@@ -3,21 +3,25 @@ const { DateTime } = require('luxon');
 const featuredProjects = ["habite", "consoul.log"]
 
 async function getRepos() {
-    const response = await fetch("/api/repos");
-    const data = await response.json();
+    try {
+        const response = await fetch("/api/repos");
+        const data = await response.json();
 
-    const repos = data.filter(repo => {
-        return (
-            // within the last 6 weeks
-            DateTime.fromISO(repo.updated_at) > DateTime.now().minus({ weeks: 8 }) &&
-            // has a description
-            repo.description !== null &&
-            // not already featured
-            !featuredProjects.includes(repo.name)
-        )
-    })
-
-    renderRepos(repos);
+        const repos = data.filter(repo => {
+            return (
+                // within the last 6 weeks
+                DateTime.fromISO(repo.updated_at) > DateTime.now().minus({ weeks: 8 }) &&
+                // has a description
+                repo.description !== null &&
+                // not already featured
+                !featuredProjects.includes(repo.name)
+            )
+        })
+    
+        renderRepos(repos);
+    } catch (error) {
+        renderError();
+    }
 }
 
 function renderRepos(repos) {
@@ -25,6 +29,17 @@ function renderRepos(repos) {
     repos.forEach(repo => {
         repoSection.append(createRepoCard(repo))
     });
+}
+
+function renderError() {
+    const repoSection = document.querySelector(".repos");
+    const errorCard = document.createElement('div');
+    errorCard.className = "item repo error";
+    errorCard.innerHTML = 
+    `<p>Sorry, couldn't get the repos from GitHub! <br>
+        Click <a href="https://github.com/theduckfliesagain" target="_blank">here</a> to go to my profile! 
+    </p>`
+    repoSection.appendChild(errorCard)
 }
 
 function createRepoCard(repo) {
